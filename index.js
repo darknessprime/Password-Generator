90cbe6be9df06822392710b3b77e00263cb73faf
darknessprime <iqbaldarvesh2016@gmail.com>
2023-10-15 01:24:37 +0530

    new features
    added password length picker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let res1 = document.getElementById('res1');
 let res2 = document.getElementById('res2');
+let len = document.getElementById('len')
 
 const char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
@@ -17,7 +18,7 @@ function reset() {
 
 function ran() {
     let password = "";
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < len.value; i++) {
         password += char[Math.floor(Math.random() * char.length)];
     }
     return password;
